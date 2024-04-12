b23d91a814ea68a5840eaf7312aa25bbc488ebfd
vietdai0504 <daithai.05042005@gmail.com>
2024-04-12 19:09:08 +0700

    add

---
+++ BT2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+class Matrix{
+    public: int N, M, K; vector<vector<int>> matrix;
+    void getInp(ifstream& fin)
+    {
+        fin>>N>>M>>K;
+        for (int i = 0; i < N;i++)
+        {
+            vector<int> row;
+            for (int j = 0;j<M;j++)
+            {
+                int a; fin>>a; row.push_back(a);
+            }
+            matrix.push_back(row);
+        }
+        // for (int i = 0;i<N;i++)
+        // {
+        //     for (int j = 0;j<M;j++)
+        //     {
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+    }
+    void maxRow_K(ofstream& fout)
+    {
+        int max = matrix[K-1][0];
+        for (int i = 0 ; i < M ; i++)
+        {
+            if (matrix[K-1][i] > max)
+            {
+                max = matrix[K-1][i];
+            }
+        }
+        fout<<max<<" ";
+    }
+    void minCol_K(ofstream& fout)
+    {
+        int min = matrix[0][K-1];
+        for (int i = 0 ; i < N ; i++)
+        {
+            if (matrix[i][K-1] < min)
+            {
+                min = matrix[i][K-1];
+            }
+        }
+        fout<<min<<" ";
+    }
+};
+int main()
+{
+    ifstream fin("InpMinMax.txt");
+    ofstream fout("OutMinMax.txt");
+    Matrix matrix;
+    matrix.getInp(fin);
+    matrix.maxRow_K(fout);
+    matrix.minCol_K(fout);
+}
